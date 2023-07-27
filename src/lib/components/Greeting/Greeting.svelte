364c02fee68a8a2f14d9108e63acca8861af9119
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-07-27 11:19:51 +0530

    replay animation only if 5 minutes have passed

--- src/lib/components/Greeting/Greeting.svelte
+++ src/lib/components/Greeting/Greeting.svelte
@@ -1,63 +1,76 @@
 <script lang="ts">
-    import GreetingSpan from "./GreetingSpan.svelte";
-    let current = "en";
-    const greetings = [
-        "en",
-        "jp",
-        "ar",
-        "gr",
-        "tm",
-        "kr",
-        "es",
-        "hi",
-        "fr",
-        "ch",
-    ];
-    function setGreeting(greetingMessage: string, i: number) {
-        setTimeout(() => {
-            current = greetingMessage;
-        }, 1000 * i);
-    }
+  import GreetingSpan from "./GreetingSpan.svelte";
+
+  let visited = localStorage.getItem("visited");
+  let current = "en";
+  const greetings = [
+    "en",
+    "jp",
+    "ar",
+    "gr",
+    "tm",
+    "kr",
+    "es",
+    "hi",
+    "fr",
+    "ch",
+  ];
+
+  function timeDiff(time: number) {
+    const now = new Date();
+    const diff = now.getTime() - time;
+    return diff / 1000;
+  }
+
+  function setGreeting(greetingMessage: string, i: number) {
+    setTimeout(() => {
+      current = greetingMessage;
+    }, 1000 * i);
+  }
+
+  if (!visited || timeDiff(JSON.parse(visited)) >= 300) {
     for (let i = 0; i <= greetings.length; i++) {
-        if (i === greetings.length) {
-            setGreeting("en", i);
-        } else {
-            setGreeting(greetings[i] as string, i);
-        }
+      if (i === greetings.length) {
+        setGreeting("en", i);
+      } else {
+        setGreeting(greetings[i] as string, i);
+      }
     }
+    localStorage.setItem("visited", JSON.stringify(new Date().getTime()));
+  }
 </script>
 
 <h2
-    class="text-xl sm:text-3xl font-extrabold inline-block text-blaze-300 italic"
+  class="text-xl sm:text-3xl font-extrabold inline-block text-blaze-300 italic"
 >
-    {#if current === "en"}
-        <GreetingSpan>Hello there!</GreetingSpan>
-    {/if}
-    {#if current === "jp"}
-        <GreetingSpan>こんにちは!</GreetingSpan>
-    {/if}
-    {#if current === "ar"}
-        <GreetingSpan>مرحبًا!</GreetingSpan>
-    {/if}
-    {#if current === "gr"}
-        <GreetingSpan>Guten tag!</GreetingSpan>
-    {/if}
-    {#if current === "tm"}
-        <GreetingSpan>வணக்கம்!</GreetingSpan>
-    {/if}
-    {#if current === "kr"}
-        <GreetingSpan>안녕하세요!</GreetingSpan>
-    {/if}
-    {#if current === "es"}
-        <GreetingSpan>Hola!</GreetingSpan>
-    {/if}
-    {#if current === "hi"}
-        <GreetingSpan>नमस्ते!</GreetingSpan>
-    {/if}
-    {#if current === "fr"}
-        <GreetingSpan>Bonjour!</GreetingSpan>
-    {/if}
-    {#if current === "ch"}
-        <GreetingSpan>你好!</GreetingSpan>
-    {/if}
+  {#if current === "en"}
+    <GreetingSpan>Hello there!</GreetingSpan>
+  {/if}
+  {#if current === "jp"}
+    <GreetingSpan>こんにちは!</GreetingSpan>
+  {/if}
+  {#if current === "ar"}
+    <GreetingSpan>مرحبًا!</GreetingSpan>
+  {/if}
+  {#if current === "gr"}
+    <GreetingSpan>Guten tag!</GreetingSpan>
+  {/if}
+  {#if current === "tm"}
+    <GreetingSpan>வணக்கம்!</GreetingSpan>
+  {/if}
+  {#if current === "kr"}
+    <GreetingSpan>안녕하세요!</GreetingSpan>
+  {/if}
+  {#if current === "es"}
+    <GreetingSpan>Hola!</GreetingSpan>
+  {/if}
+  {#if current === "hi"}
+    <GreetingSpan>नमस्ते!</GreetingSpan>
+  {/if}
+  {#if current === "fr"}
+    <GreetingSpan>Bonjour!</GreetingSpan>
+  {/if}
+  {#if current === "ch"}
+    <GreetingSpan>你好!</GreetingSpan>
+  {/if}
 </h2>
