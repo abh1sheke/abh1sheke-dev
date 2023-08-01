1cdcc47dc0cb6ddc1cb4adceeca2cdafa6722299
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-08-01 10:19:53 +0530

    add dummy blog-post and filter out drafted posts

---
+++ src/content/blog-posts/test.md
@@ -0,0 +1,8 @@
+---
+title: hello world!
+readTime: 1 min
+date: Aug 1, 2023
+draft: true
+---
+
+Hey guys whats up
--- src/content/config.ts
+++ src/content/config.ts
@@ -6,6 +6,7 @@ const blogCollection = defineCollection({
     title: z.string(),
     readTime: z.string(),
     date: z.string(),
+    draft: z.boolean(),
   }),
 });
 
